edc4d72e970677f86e522f468008c8a0cd557a50
unknown <nayefkanaan03@gmail.com>
2022-06-13 21:41:01 +0300

    Responsive

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,13 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
     <>
       <Head>
         <title> Probably </title>
+        <meta charSet="UTF-8" />
+        <meta
+          name="keywords"
+          content="Crypto, meta, metaverse, web3, wbe3.0, MENA, middle east, design, creative, marketing, ethereum, bitcoin, litecoin, cryptocurrencies, blockchain, digital, wallet, smart contract, nayef kanaan, nabil kanaan, nabil, kanaan, probably, cryptomena, prbly, nayef, kanaan, naif, excelsior"
+        />
+        <meta name="author" content="Excelsior Studios, Nayef Kanaan" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
       <AuthProvider session={session}>
